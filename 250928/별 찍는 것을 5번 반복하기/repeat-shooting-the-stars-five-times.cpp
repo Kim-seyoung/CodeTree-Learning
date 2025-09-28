a016678abfde7615eb898bef088da00248e90a94
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-09-28 12:39:05 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250928/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printStar(){
+    cout << "**********" << endl;
+}
+
+int main() {
+    // Please write your code here.
+    for(int i = 0; i < 5; i++) {
+        printStar();
+    }
+    return 0;
+}
