1a88e65a6f7bb85e77234b306fb2477665c0ea08
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-09-29 20:28:27 +0900

    Title: 절댓값으로 변경, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250929/절댓값으로 변경/find-the-absolute-value.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[50];
+
+void func(int arr[]) {
+    for (int i = 0; i < n; i++) {
+        if(arr[i] < 0)
+            arr[i] *= -1;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+
+    func(arr);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
