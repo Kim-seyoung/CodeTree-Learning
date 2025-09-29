4fe55cd7e8530a95e7aa555f8536eebf8291e6d5
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-09-29 20:52:49 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250929/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string text;
+string pattern;
+
+int isSubStr() {
+    int idx = -1;
+    for(int i = 0; i < text.length() + 1 - pattern.length(); i++) {
+        bool exist = true;
+        for(int j = 0; j < pattern.length(); j++) {
+            if(text[i+j] != pattern[j])
+            {
+                exist = false;
+                break;
+            }
+        }
+        if(exist) idx = i;
+    }
+    return idx;
+}
+
+int main() {
+    cin >> text;
+    cin >> pattern;
+
+    // Please write your code here.
+    int result = isSubStr();
+    cout << result;
+    return 0;
+}
