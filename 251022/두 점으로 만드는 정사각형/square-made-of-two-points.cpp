3f4598d3e8d6b32e609356a90b7daa3f50ca15f9
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-10-22 17:20:23 +0900

    Title: 두 점으로 만드는 정사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251022/두 점으로 만드는 정사각형/square-made-of-two-points.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+    int a1, b1, a2, b2; cin >> a1 >> b1 >> a2 >> b2;
+
+    int start_x1; 
+    int start_y1; 
+    int start_x2; 
+    int start_y2;
+
+    if(x1 < a1)
+        start_x1 = x1;
+    else 
+        start_x1 = a1;
+
+    if(x2 < a2)
+        start_x2 = a2;
+    else 
+        start_x2 = x2;
+
+    if(y1 < b1)
+        start_y1 = y1;
+    else 
+        start_y1 = b1;
+    
+    if(y2 < b2)
+        start_y2 = b2;
+    else 
+        start_y2 = y2;
+    
+    if((start_x2 - start_x1) >= (start_y2 - start_y1))
+        cout << (start_x2 - start_x1) * (start_x2 - start_x1);
+    else    
+        cout << (start_y2 - start_y1) * (start_y2 - start_y1);
+
+    return 0;
+}
