ba86686273a504b9abf2002349de88a7c5c8914f
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-10-23 23:02:36 +0900

    Title: Carry 피하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251023/Carry 피하기 2/escaping-carry-2.cpp
+++ 251023/Carry 피하기 2/escaping-carry-2.cpp
@@ -31,8 +31,8 @@ int main() {
     
     int max_sum = -1;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            for(int k = 0; k < n; k++) {
+        for(int j = i+1; j < n; j++) {
+            for(int k = j+1; k < n; k++) {
                 if(Carry(a[i], a[j], a[k]))
                     continue;
                 else
