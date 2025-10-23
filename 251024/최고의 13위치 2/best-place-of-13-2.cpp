406aec228312bacaa5f195e28623948dbb8cc365
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-10-24 00:26:55 +0900

    Title: 최고의 13위치 2, Time: 18ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251024/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n; cin >> n;
+    int a[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) 
+            cin >> a[i][j];
+    }
+
+    int max_cnt1 = 0; int x, y;
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j < n-1; j++) {
+            int cnt = 0;
+            if(a[i][j-1] == 1) cnt++; 
+            if(a[i][j] == 1) cnt++; 
+            if(a[i][j+1] == 1) cnt++; 
+
+            if(max_cnt1 < cnt) {
+                max_cnt1 = cnt;
+                x = i; y = j;
+            }
+        }
+    }
+
+    a[x][y-1] = 2;
+    a[x][y] = 2;
+    a[x][y+1] = 2;
+
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 0; j < n; j++) 
+    //         cout << a[i][j] << " ";
+    //     cout << endl;
+    // }
+
+    int max_cnt2 = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j < n-1; j++) {
+            if(a[i][j-1] == 2 || a[i][j] == 2 || a[i][j+1] == 2)
+                continue;
+            
+            int cnt = 0;
+            if(a[i][j-1] == 1) cnt++; 
+            if(a[i][j] == 1) cnt++; 
+            if(a[i][j+1] == 1) cnt++; 
+
+            if(max_cnt2 < cnt) {
+                max_cnt2 = cnt;
+            }
+        }
+    }
+
+    cout << max_cnt1 + max_cnt2;
+    return 0;
+}
