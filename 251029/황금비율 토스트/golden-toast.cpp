0c3bc7b9db0f37ea9725b9ad0ca68ab9746297bd
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-10-29 22:38:42 +0900

    Title: 황금비율 토스트, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 251029/황금비율 토스트/golden-toast.cpp
+++ 251029/황금비율 토스트/golden-toast.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
-#include <list>
 #include <string>
+#include <list>
+ 
 using namespace std;
 
-int main() {
-    // Please write your code here.
+int n, m;
+
+string s;
+
+int main(){
+    // 입력:
+    cin >> n >> m;
+    cin >> s;
+    
+    // 연결리스트 정의
     list<char> l;
-    int n, m; cin >> n >> m;
-    string bread; cin >> bread;
-    char ch; list<char>::iterator it; 
-    for(int i = 0; i < bread.length(); i++) {
-        l.push_back(bread[i]);
-    }
-    it = l.end();
-    for(int i = 0; i < m; i++) {
-        cin >> ch;
-        if(ch == 'L') {
-            it--;
-        } else if(ch == 'R') {
-            it++;
-        } else if(ch == 'D') {
-            it = l.erase(it);
-        } else if(ch == 'P') {
-            char a;
-            cin >> a;
-            l.insert(it, a); 
+    for(int i = 0; i < s.length(); i++)
+        l.push_back(s[i]);  
+    // iterator 정의
+    list<char>::iterator it = l.end();
+    
+    while(m--) { 
+        char command;
+        cin >> command;
+        
+        if(command == 'L') {
+            if(it != l.begin())   // 빵들의 맨 앞이 아니라면
+                it--;             // 앞으로 이동합니다.
+        } 
+        else if(command == 'R') {
+            if(it != l.end())     // 빵들의 맨 뒤가 아니라면
+                it++;             // 뒤로 이동합니다.
         }
+        else if(command == 'D') {
+            if(it != l.end())     // 빵들의 맨 뒤가 아니라면
+                it = l.erase(it); // 바로 뒤에 있는 빵을 제거합니다.
+        }
+        else if(command == 'P') {
+            char c;
+            cin >> c;
+            l.insert(it, c);      // 가리키는 위치에 문자 c를 추가합니다.
+        }    
     }
-    for(it = l.begin(); it != l.end(); it++) { 
-        cout << *it;     
-    } 
-    return 0;
+    
+    // 출력:
+    for (list<char>::iterator it = l.begin(); it != l.end(); it++) {
+        cout << *it;
+    }
+    return 0;     
 }
