b1feed43b393aeab7d17d1bacdda39cef8006bda
Kim-seyoung <116957117+Kim-seyoung@users.noreply.github.com>
2025-10-30 11:29:49 +0900

    Title: 거품 정렬 구현, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251030/거품 정렬 구현/implement-bubble-sort.cpp
+++ 251030/거품 정렬 구현/implement-bubble-sort.cpp
@@ -10,16 +10,15 @@ int main() {
     bool sorted = true;
     do {
         sorted = true;
-        for(int i = 0; i < n-1; i++) {
-            for(int j = 0; j < n-1-i; j++) {
-                if(a[j] > a[j+1]) {
-                    sorted = false;
-                    int tmp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = tmp;
-                }
+      
+        for(int j = 0; j < n-1; j++) {
+            if(a[j] > a[j+1]) {
+                sorted = false;
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
             }
-        }
+        }    
     } while(!sorted);
 
     for(int i = 0; i < n; i++) 
